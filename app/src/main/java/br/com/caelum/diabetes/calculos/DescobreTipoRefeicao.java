b4aa26c604e6f4c087c3e9d43bdefcde02dd1221
Fernanda Bernardo <fernandabernardo94@gmail.com>
2016-04-16 17:36:30 -0300

    refactor variable

--- app/src/main/java/br/com/caelum/diabetes/calculos/DescobreTipoRefeicao.java
+++ app/src/main/java/br/com/caelum/diabetes/calculos/DescobreTipoRefeicao.java
@@ -8,12 +8,13 @@ public class DescobreTipoRefeicao {
 	TipoRefeicao tipoRefeicao;
 	public DescobreTipoRefeicao() {
 		DateTime dataAtual = new DateTime();
-		if (dataAtual.getHourOfDay() >= 4 && dataAtual.getHourOfDay() < 10) tipoRefeicao = TipoRefeicao.CAFE_DA_MANHA;
-		if (dataAtual.getHourOfDay() >= 10 && dataAtual.getHourOfDay() < 12) tipoRefeicao = TipoRefeicao.LANCHE_DA_MANHA;
-		if (dataAtual.getHourOfDay() >= 12 && dataAtual.getHourOfDay() < 15) tipoRefeicao = TipoRefeicao.ALMOCO;
-		if (dataAtual.getHourOfDay() >= 15 && dataAtual.getHourOfDay() < 18) tipoRefeicao = TipoRefeicao.LANCHE_DA_TARDE;
-		if (dataAtual.getHourOfDay() >= 18 && dataAtual.getHourOfDay() < 24) tipoRefeicao = TipoRefeicao.JANTAR;
-		if (dataAtual.getHourOfDay() >= 24 || dataAtual.getHourOfDay() < 4) tipoRefeicao = TipoRefeicao.CEIA;
+		int hourOfDay = dataAtual.getHourOfDay();
+		if (hourOfDay >= 4 && hourOfDay < 10) tipoRefeicao = TipoRefeicao.CAFE_DA_MANHA;
+		if (hourOfDay >= 10 && hourOfDay < 12) tipoRefeicao = TipoRefeicao.LANCHE_DA_MANHA;
+		if (hourOfDay >= 12 && hourOfDay < 15) tipoRefeicao = TipoRefeicao.ALMOCO;
+		if (hourOfDay >= 15 && hourOfDay < 18) tipoRefeicao = TipoRefeicao.LANCHE_DA_TARDE;
+		if (hourOfDay >= 18 && hourOfDay < 24) tipoRefeicao = TipoRefeicao.JANTAR;
+		if (hourOfDay >= 24 || hourOfDay < 4) tipoRefeicao = TipoRefeicao.CEIA;
 	}
 	
 	public TipoRefeicao getTipoRefeicao() {
