3e2b1aa3a621a6e91a5edf405a4f62595f61261c
Fernanda Bernardo <fernandabernardo94@gmail.com>
2017-08-07 21:44:46 -0300

    adiciona migration para coluna madrugada

--- app/src/main/java/br/com/caelum/diabetes/dao/DbHelper.java
+++ app/src/main/java/br/com/caelum/diabetes/dao/DbHelper.java
@@ -1,9 +1,16 @@
 package br.com.caelum.diabetes.dao;
 
-import java.sql.SQLException;
-
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
+
 import br.com.caelum.diabetes.model.AlimentoFisico;
 import br.com.caelum.diabetes.model.AlimentoVirtual;
 import br.com.caelum.diabetes.model.DadosMedicos;
@@ -12,30 +19,21 @@ import br.com.caelum.diabetes.model.Lembrete;
 import br.com.caelum.diabetes.model.Paciente;
 import br.com.caelum.diabetes.model.Refeicao;
 
-import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
-import com.j256.ormlite.dao.RuntimeExceptionDao;
-import com.j256.ormlite.support.ConnectionSource;
-import com.j256.ormlite.table.DatabaseTableConfig;
-import com.j256.ormlite.table.TableUtils;
-
 public class DbHelper extends OrmLiteSqliteOpenHelper{
 
 	private static final String DATABASE = "Diabetes";
-	
-	AlimentoFisicoDao alimentoFisicoDao;
 
 	public Context context;
-    public ConnectionSource connectionSource;
 
 	public DbHelper(Context context) {
-		super(context, DATABASE, null, 1);
+		super(context, DATABASE, null, 2);
 		this.context = context;
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
-        this.connectionSource = connectionSource;
 		try {
+			Log.i(DbHelper.class.getName(), "onCreate");
 			TableUtils.createTable(connectionSource, Paciente.class);
 			TableUtils.createTable(connectionSource, DadosMedicos.class);
 			TableUtils.createTable(connectionSource, Refeicao.class);
@@ -45,21 +43,22 @@ public class DbHelper extends OrmLiteSqliteOpenHelper{
 			TableUtils.createTable(connectionSource, Glicemia.class);
 
 		} catch (SQLException e) {
+			Log.e(DbHelper.class.getName(), "Can't create database", e);
 			throw new RuntimeException(e);
 		}
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion) {
-        this.connectionSource = connectionSource;
-		try {
-			TableUtils.dropTable(connectionSource, Paciente.class, true);
-			onCreate(db, connectionSource);
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
+		Log.i(DbHelper.class.getName(), "onUpgrade");
+		switch (oldVersion) {
+			case 1:
+				String sql = "ALTER TABLE dadosmedicos ADD column madrugada REAL";
+				db.execSQL(sql);
+				Log.i(DbHelper.class.getName(), "run migration 1");
 		}
 	}
-	
+
 	public <T> RuntimeExceptionDao<T, Integer> getSimpleDataDao(Class<T> clazz) {
 		return getRuntimeExceptionDao(clazz);
 	}
