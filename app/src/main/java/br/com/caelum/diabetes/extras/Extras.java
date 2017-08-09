f20d3ed21195de2a44c64dfbfef5a3170128e5ca
Fernanda Bernardo <fernandabernardo94@gmail.com>
2017-08-09 18:15:51 -0300

    adiciona observação na planilha de excel

--- app/src/main/java/br/com/caelum/diabetes/extras/Extras.java
+++ app/src/main/java/br/com/caelum/diabetes/extras/Extras.java
@@ -7,18 +7,20 @@ public class Extras {
     public static final String ALMOCO = "Almoço";
     public static final String LANCHE_DA_TARDE = "Lanche da Tarde";
     public static final String JANTAR = "Jantar";
-    public static final String CEIA = "Ceia";
+	public static final String CEIA = "Ceia";
     public static final String MADRUGADA = "Madrugada";
 
-//	Configurações do Perfil    
+	public static final String OBSERVACAO = "Observação";
+
+	//	Configurações do Perfil
     public static final String DIABETES_1 = "Tipo 1";
-    public static final String DIABETES_2 = "Tipo 2";
+	public static final String DIABETES_2 = "Tipo 2";
 	public static final String CONTINUO = "Contínuo";
 	public static final String FATOR_CORRECAO = "Correção";
 	public static final String CORRECAO = "Fator de Correção";
+
 	public static final String GLICEMIA_ALVO = "Glicemia Alvo";
-	
-//	Unidades de Medida
+	//	Unidades de Medida
 	public static final String COLHER_DE_SOPA = "Colher de Sopa";
 	public static final String COLHER_DE_CHA = "Colher de Chá";
 	public static final String CONCHA_MEDIA = "Concha Média";
@@ -27,10 +29,10 @@ public class Extras {
 	public static final String UNIDADE_PEQUENA = "Unidade Pequena";
 	public static final String ESCUMADEIRA = "Escumadeira";
 	public static final String XICARA = "Xícara";
-	public static final String COPO = "Copo";
 
-    public static final String PREFS_NAME= "Configuracao";
-    public static final String PREFS_NAME_INSULINA_CORRECAO = "configInsulinaCorrecao";
+	public static final String COPO = "Copo";
+	public static final String PREFS_NAME= "Configuracao";
+	public static final String PREFS_NAME_INSULINA_CORRECAO = "configInsulinaCorrecao";
 	public static final String PREFS_NAME_FATOR_CORRECAO = "configFatorCorrecao";
 	public static final String PREFS_NAME_GLICEMIA_ALVO = "configGlicemiaAlvo";
 }
